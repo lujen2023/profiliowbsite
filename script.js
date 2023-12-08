f88cb59c369a107f6c620fec67112013becd1661
Lujen Al-Radwan <136607423+lujen2023@users.noreply.github.com>
2023-12-08 19:11:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links"); // targeting two element
+    const icon = document.querySelector(".phone-icon"); // targeting two element
+    menu.classList.toggle("open"); //when ever we click it it will add or remove the open class which has styling
+    icon.classList.toggle("open"); //when ever we click it it will add or remove the open class which has styling
+}
+
